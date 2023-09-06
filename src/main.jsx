98b5a769e551811714569c45507cdcf1ab772991
NIEOKY <56267850+NIEOKY@users.noreply.github.com>
2023-09-06 09:43:52 -0600

    ???

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <p className="text-black w-full text-center text-2xl font-bold">
+      cofe warehouse optimization using tabu search
+    </p>
     <App />
   </React.StrictMode>
 );
