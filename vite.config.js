66d0914f94b871fddc63f71c15d7a52792580dec
NIEOKY <56267850+NIEOKY@users.noreply.github.com>
2023-09-06 09:26:00 -0600

    qfix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cafeterias/',
+  base: '/',
   assetsInclude: ['cafes.csv'],
   server: {
     open: true, // Opcional: abrir automáticamente la aplicación en el navegador
