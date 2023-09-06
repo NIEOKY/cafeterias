c8661290c56ae2e29f29a4615175f1ebdecfc77c
NIEOKY <56267850+NIEOKY@users.noreply.github.com>
2023-09-06 09:27:58 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/vite-deploy/',
   assetsInclude: ['cafes.csv'],
   server: {
     open: true, // Opcional: abrir automáticamente la aplicación en el navegador
