0cfc998904c774b48759a018b30d52d000490470
NIEOKY <56267850+NIEOKY@users.noreply.github.com>
2023-09-06 09:29:46 -0600

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy/',
+  base: '/cafeterias/',
   assetsInclude: ['cafes.csv'],
-  server: {
-    open: true, // Opcional: abrir automáticamente la aplicación en el navegador
-  },
 });
